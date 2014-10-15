57b5fc6989aae7d60e1bc16060f5f064c4e796f3
Craig Walls <cwalls@gopivotal.com>
2014-10-15 09:38:19 -0500

    Polish

--- spring-sync/src/main/java/org/springframework/sync/diffsync/PersistenceCallbackRegistry.java
+++ spring-sync/src/main/java/org/springframework/sync/diffsync/PersistenceCallbackRegistry.java
@@ -18,20 +18,47 @@ package org.springframework.sync.diffsync;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.sync.Patch;
+
+/**
+ * Registry for looking up {@link PersistenceCallback}s to be used in the course of applying a {@link Patch} via
+ * Differential Synchronization.
+ * 
+ * @author Craig Walls
+ */
 public class PersistenceCallbackRegistry {
 
 	private Map<String, PersistenceCallback<?>> persistenceCallbacks = new HashMap<String, PersistenceCallback<?>>();
 	
 	public PersistenceCallbackRegistry() {}
 	
+	/**
+	 * Adds a {@link PersistenceCallback} to the registry with a key that is pluralized by the pluralize() method.
+	 * @param persistenceCallback the {@link PersistenceCallback} to add to the registry.
+	 */
 	public void addPersistenceCallback(PersistenceCallback<?> persistenceCallback) {
 		Class<?> entityType = persistenceCallback.getEntityType();
-		String key = entityType.getSimpleName().toLowerCase() + "s"; // TODO - Naive approach to pluralization
+		String key = pluralize(entityType.getSimpleName());
 		persistenceCallbacks.put(key, persistenceCallback);
 	}
 	
+	/**
+	 * Looks up a {@link PersistenceCallback} from the registry.
+	 * @param key the key that the {@link PersistenceCallback} has been registered under.
+	 * @return the {@link PersistenceCallback}
+	 */
 	public PersistenceCallback<?> findPersistenceCallback(String key) {
 		return persistenceCallbacks.get(key);
+	}
+	
+	/**
+	 * Pluralizes an entity's type name. Default implementation is to naively add an 's' to the end of the given String.
+	 * Override to implement a more elegant pluralization technique.
+	 * @param entityTypeName the entity type name to be pluralized.
+	 * @return the pluralized type name.
+	 */
+	protected String pluralize(String entityTypeName) {
+		return entityTypeName.toLowerCase() + "s";
 	}
 	
 }
